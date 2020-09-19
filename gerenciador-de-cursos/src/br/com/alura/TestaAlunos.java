40d16d79bb80623aa111d0afda44074c986d791f
fjp-j <fjp-j@192.168.0.4>
2020-09-19 17:44:02 -0300

    gerenciador-de-cursos

---
+++ gerenciador-de-cursos/src/br/com/alura/TestaAlunos.java
@@ -0,0 +1,38 @@
+package br.com.alura;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class TestaAlunos {
+	public static void main(String[] args) {
+		Collection<String> alunos = new HashSet<>();
+		alunos.add("Rodrigo Turini");
+		alunos.add("Alberto Souza");
+		alunos.add("Nico Steppat");
+		alunos.add("Sergio Lopes");
+		alunos.add("Renan Saggio");
+		alunos.add("Mauricio Aniche");
+		alunos.add("Alberto Souza");
+		
+		boolean fernandoEstaMatriculado = alunos.contains("Fernando Perez");
+		alunos.remove("Sergio Lopes");
+		System.out.println(fernandoEstaMatriculado);
+		
+		System.out.println(alunos.size());
+		
+		for (String aluno : alunos) {
+			System.out.println(aluno);
+		}
+		
+		alunos.forEach(aluno -> {
+			System.out.println(aluno);
+		});
+		
+		System.out.println(alunos);
+		
+		List<String> alunosEmLista = new ArrayList<>(alunos);
+	}
+
+}
